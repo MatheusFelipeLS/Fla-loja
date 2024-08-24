dcc559663f197e2265a624b1d3401c6244501286
Matheus Felipe <mathfelpessoal@gmail.com>
2024-08-24 20:15:29 -0300

    Aperfeiçoando o front

--- flamengo/fla_loja/urls.py
+++ flamengo/fla_loja/urls.py
@@ -7,6 +7,6 @@ app_name = "fla_loja"
 
 urlpatterns = [
   path('', views.index, name='home'),
-  path('product/<str:nick>', views.get_product_by_name, name='product'),
+  path('product/<str:_id>', views.get_product_by_name, name='product'),
   path('data/', views.product_manager)
 ]
