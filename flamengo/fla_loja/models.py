fec791c48f5a8afbdfca624a72e702a78097d9bc
Matheus Felipe <mathfelpessoal@gmail.com>
2024-08-26 01:56:14 -0300

    a

--- flamengo/fla_loja/models.py
+++ flamengo/fla_loja/models.py
@@ -6,6 +6,7 @@ class Client(models.Model):
   cpf = models.CharField(max_length=14, default='')
   phone = models.CharField(max_length=15, default='')
   email = models.EmailField(default='')
+  activate = models.BooleanField(default=True)
   
   
 class Employee(models.Model):
@@ -13,7 +14,7 @@ class Employee(models.Model):
   wage = models.FloatField(default=0.0)
   sales_count = models.IntegerField(default=0)
   photo = models.ImageField(default='', upload_to='flamengo/fla_loja/static/fla_loja/employee', height_field=None, width_field=None, max_length=100)
-
+  activate = models.BooleanField(default=True)
   
 class Product(models.Model):
   name = models.CharField(max_length=50, default='')
@@ -21,7 +22,7 @@ class Product(models.Model):
   price = models.FloatField(default=0.0)
   quantity_in_stock = models.IntegerField(default=0)
   image = models.ImageField(default='', upload_to='flamengo/fla_loja/static/fla_loja/products', height_field=None, width_field=None, max_length=100)
-  
+  activate = models.BooleanField(default=True)
   
 class Sale(models.Model):
   id_client = models.ForeignKey(Client, default='', on_delete=models.SET_NULL, null=True)
@@ -29,3 +30,5 @@ class Sale(models.Model):
   id_employee = models.ForeignKey(Employee, on_delete=models.SET_NULL, null=True)
   data = models.DateField("Date purchased")
   quantity = models.IntegerField(default=0)
+
+  
